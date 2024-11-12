68a6a41f10d172301cef430e26015cc1e10cdb0c
AlexanderBurnstein <127631079+AlexanderBurnstein@users.noreply.github.com>
2024-11-12 12:37:00 -0800

    Add files via upload

--- hw3cs52maxton.cpp
+++ hw3cs52maxton.cpp
@@ -1,46 +1,65 @@
 #include <iostream>
+// Need access to the string type
 #include <string>
-
 
 int main()
 {   
     std::string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",};
-    int userIncome[12];
-    int maxtaxIncome[6] = {9325, 28625, 53950, 99750, 225050, 1700};
+    int userMonthlyIncome[12];
+    int taxBracket[6] = {9325, 28625, 53950, 99750, 225050, 1700};
     double taxPercent[7] = {0.1, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396};
 
-    int runningSum = 10000;
-    double totalTax = 0;
-    int runningtaxTotal = 0;
-    int taxOwed = 0;
-    // For loop for months
+    // total earnings
+    int totalIncome = 0;
+    // var to store tax rate %
+    double taxRatePercent = 0;
+    // var to store the tax sum amounts
+    int totalTaxOwed = 0;
+    // var to store tax remainder
+    int taxRemainder = 0;
+
+    std::cout << "ITP tax calculator:" << std::endl;
+    
+    // Total income calculator
     for(int i = 0; i < 12;i++)
     {
-        // std::cout << "Enter income for " << months[i] << ": $";
-        // std::cin >> userIncome[i];
-        //runningSum = runningSum + userIncome[i];
+        std::cout << "Enter income for " << months[i] << ": $";
+        std::cin >> userMonthlyIncome[i];
+        // could have used a local variable to store each month of input
+        totalIncome = totalIncome + userMonthlyIncome[i];
     }
-    // For loop for income limits and tax percent
+
+    // Set Tax Remainder to total income
+    taxRemainder = totalIncome;
+
+    // Tax calculations
     for(int i = 0; i < 6;i++)
-    {
-        if(maxtaxIncome[i] > runningSum)
+    { 
+        if(taxBracket[i] < taxRemainder)
         {
-            runningtaxTotal = maxtaxIncome[i]; 
+            totalTaxOwed += taxBracket[i] * taxPercent[i];
+            taxRemainder = taxRemainder - taxBracket[i];
+
+            if (i == 5)
+            {
+                totalTaxOwed += taxRemainder * taxPercent[i + 1];    
+            }   
         }
+        else
+        {
+            totalTaxOwed += taxRemainder * taxPercent[i];
+            break;
+        }     
     }
 
-    // 1. create variable that stores remainder of the maxtaxincome as we go through each tax bracket
-    // 2. if the remaining amount is greater than 0 move to next tax bracket/element - tax it - then add it to the runningtaxtotal
-    // 3. if we have any money left over tax it and add it to the runningtaxtotal.
-    // 4. display the tax total and calculate tax rate
+    //tax rate percent calc
+    taxRatePercent = ((double)totalTaxOwed / totalIncome) * 100;
     
-    std::cout << runningSum << std::endl;
-    std::cout << totalTax << std::endl;
-    std::cout << runningtaxTotal << std::endl;
-    std::cout << taxOwed << std::endl;
-    
-    //std::cout << "Your total income is $" << runningSum << std::endl;
-
+    //cout statements
+    std::cout << "******************************" << std::endl;
+    std::cout << "Your total income is $" << totalIncome << std::endl;
+    std::cout << "You owe taxes in the amount of $" << totalTaxOwed << std::endl;
+    std::cout << "Your effective tax rate is " << taxRatePercent << "%" << std::endl;
 
     return 0;
 }
